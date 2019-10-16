8ffd24f704bde6fbef2e18c3b13298097fb74e0e
Peter de Ruijter <hello@thisiswho.im>
2019-10-16 15:33:17 +0200

    Enable emulator.

--- config/test.exs
+++ config/test.exs
@@ -1,6 +1,7 @@
 use Mix.Config
 
 config :goth,
-  json: "config/test-credentials.json" |> Path.expand |> File.read!
+  json: "config/test-credentials.json" |> Path.expand() |> File.read!()
 
 config :kane, :token, Kane.TestToken
+config :kane, :local, true
